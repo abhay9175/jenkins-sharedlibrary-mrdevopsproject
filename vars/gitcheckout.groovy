2f117f6714d3c7de06e24a4872228cc16a4a817b
abhay9175 <113837646+abhay9175@users.noreply.github.com>
2023-05-20 17:03:04 +0530

    Update gitcheckout.groovy

---
+++ vars/gitcheckout.groovy
@@ -0,0 +1,8 @@
+def call(Map stageParams) {
+ 
+    gitcheckout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
